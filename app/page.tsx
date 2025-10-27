c23e6bafe7dc0cf075a8eda734114a04238b9f2e
Reqwiew <dobryjartem738@gmail.com>
2025-10-27 17:00:20 +0200

    add dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,52 @@
+'use client';
 import Image from "next/image";
+import RequestsDashboard from "@/app/components/dashboard1";
+import logo from "./assets/logo.svg";
+import topleft from "./assets/circles_top.svg";
+import botright from "./assets/circles_bottom.svg";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file1.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+export default function Main() {
+    return (
+        <div className="relative min-h-screen bg-white overflow-hidden">
+            <div className="absolute inset-0 z-1">
+                <Image
+                    src={topleft}
+                    alt="Top Left"
+                    width={500}
+                    height={500}
+                    className="absolute top-0 left-0 object-contain pointer-events-none select-none opacity-30"
+                />
+                <Image
+                    src={botright}
+                    alt="Bottom Right"
+                    width={500}
+                    height={500}
+                    className="absolute bottom-0 right-0 object-contain pointer-events-none select-none opacity-30"
+                />
+            </div>
+
+
+            <div className="w-full h-36 flex flex-col items-center relative z-10">
+                <div className="bg-[#FF5A00] h-18 w-full text-xl text-white flex justify-center items-center">
+                    <p>InsurTech-платформа для логистики</p>
+                </div>
+                <div className="w-full flex justify-left bg-white">
+                    <Image
+                        src={logo}
+                        alt="InsurTech logo"
+                        width={160}
+                        height={60}
+                        priority
+                        className="p-6"
+                    />
+                </div>
+            </div>
+
+            <div className="w-full flex justify-center relative z-10">
+                <div className="w-4/6">
+                    <RequestsDashboard/>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
